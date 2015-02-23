57984f36a3875adb85893f1ebde36caf0534615b
Rohail Najam <rohail_najam@hotmail.com>
2015-02-23 13:17:21 +0500

    Clean Up

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -7,16 +7,17 @@ public class Ball : MonoBehaviour {
 	public Text displayRound;
 	public Text displayScore;
 	public Text displayFinalScore;
-	public Text debugText;
+	public BoxCollider boundingBox;
 	private Vector3 screenPoint ;
 	private Vector3 offset;
-	public  Transform[] PinSet = new Transform[10];
+	public  GameObject[] PinSet = new GameObject[10];
 	private Vector3[] PinPosition = new Vector3[10];
 	private Quaternion[]  PinRotation = new Quaternion[10];
 	private Vector3 defaultCameraPosition;
 	private Quaternion defaultCameraRotation;
 	private Vector3 defaultPosition;
 	private Vector3 LastPinDefaultPosition;
+	private bool soundFlag;
 	public Camera cam1;
 	private int currentTurn = 0;
 	private int totalScore = 0;
@@ -26,19 +27,20 @@ public class Ball : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-
+		Physics.IgnoreCollision (this.collider, boundingBox);
 		defaultCameraPosition = cam1.transform.position;
 		defaultCameraRotation = cam1.transform.rotation;
-		LastPinDefaultPosition = PinSet[PinSet.Length - 1].position;
+		LastPinDefaultPosition = PinSet[PinSet.Length - 1].transform.position;
 		defaultPosition = transform.position;
 		NumberOfGames = 1;
 		totalScore = 0;
 		FinalScore = 0;
+		soundFlag = false;
 		displayScore.text = displayFinalScore.text = "";
 		displayRound.text = NumberOfGames+"";
 		for (int i = 0; i < 10; i++) {
-						PinPosition [i] = PinSet [i].position;
-						PinRotation [i] = PinSet [i].rotation;
+			PinPosition [i] = PinSet [i].transform.position;
+			PinRotation [i] = PinSet [i].transform.rotation;
 
 				}
 	}
@@ -47,35 +49,48 @@ public class Ball : MonoBehaviour {
 	void Update () {
 	
 
+
 	}
+
+	void OnCollisionEnter(Collision c)
+	{
+
+		if (c.gameObject.tag == "PIN" && !soundFlag) {
+			cam1.GetComponent<AudioSource>().Play();
+			soundFlag = true;
+
+				}
+	}
+
 
 
 
 	void FixedUpdate()
 	{
+				
 				if (NumberOfGames > 5) {
 						Application.LoadLevel ("ScoreMenu");
 				}
 
-				if (transform.rigidbody.velocity.magnitude > 2 && cam1.enabled) {
+//				if (transform.rigidbody.velocity.magnitude > 2 && cam1.enabled) {
+//
+//
+//						if (Vector3.Distance (cam1.transform.position, transform.position) > 7 && Vector3.Distance (cam1.transform.position, LastPinDefaultPosition) > 25) {
+//								cam1.transform.position = new Vector3 (cam1.transform.position.x,
+//				                                      Mathf.Lerp (defaultCameraPosition.y, defaultCameraPosition.y + 2f,
+//				             Vector3.Distance (cam1.transform.position, transform.position) / Vector3.Distance (cam1.transform.position, LastPinDefaultPosition)),
+//				                                      transform.position.z - 7);
+//						}
+//		
 
 
-						if (Vector3.Distance (cam1.transform.position, transform.position) > 7 && Vector3.Distance (cam1.transform.position, LastPinDefaultPosition) > 25) {
-								cam1.transform.position = new Vector3 (cam1.transform.position.x,
-				                                      Mathf.Lerp (defaultCameraPosition.y, defaultCameraPosition.y + 2f,
-				             Vector3.Distance (cam1.transform.position, transform.position) / Vector3.Distance (cam1.transform.position, LastPinDefaultPosition)),
-				                                      transform.position.z - 7);
-						}
-		
 
-
-
-				}
+//				}
 		if (Vector3.Distance (defaultPosition, transform.position) > Vector3.Distance (defaultPosition, LastPinDefaultPosition )* 0.1f
 					     && rigidbody.velocity.magnitude < .1f){
 								reset();
 					}
-		if(Vector3.Distance (defaultPosition, transform.position) > Vector3.Distance (defaultPosition, LastPinDefaultPosition * 5f)) {
+		if(Vector3.Distance (defaultPosition, transform.position) > Vector3.Distance (defaultPosition, LastPinDefaultPosition *2.1f)) {
 			reset ();
 		}
 		}
@@ -90,7 +105,7 @@ public class Ball : MonoBehaviour {
 		transform.position = defaultPosition;
 		cam1.transform.position = defaultCameraPosition;
 		cam1.transform.rotation = defaultCameraRotation;
-
+		soundFlag = false;
 		
 		rigidbody.velocity = Vector3.zero;
 		rigidbody.angularVelocity = Vector3.zero;
@@ -137,32 +152,36 @@ public class Ball : MonoBehaviour {
 			NumberOfGames++;
 			displayRound.text = NumberOfGames+"";
 		for (int i = 0; i < 10; i++) {
-			PinSet[i].position = PinPosition[i];
-			PinSet[i].rotation = PinRotation[i];
+			PinSet[i].transform.position = PinPosition[i];
+			PinSet[i].transform.rotation = PinRotation[i];
 
-			PinSet[i].GetChild(0).transform.renderer.enabled = true;
-			PinSet[i].transform.collider.enabled = true;
+//			PinSet[i].transform.GetChild(0).transform.renderer.enabled = true;
+//			PinSet[i].transform.collider.enabled = true;
 
 			PinSet[i].rigidbody.velocity = Vector3.zero;
 			PinSet[i].rigidbody.angularVelocity = Vector3.zero;
-			
+			PinSet[i].SetActive(true);
 		}
 	}
 
 	int GetFallenPinsCount(){
 		int score = 0;
 		for (int i = 0; i < 10; i++) {
-			Vector3 cuurentRotation = PinSet[i].rotation.eulerAngles;
+			Vector3 cuurentRotation = PinSet[i].transform.rotation.eulerAngles;
 			Vector3 defaultRotation = PinRotation[i].eulerAngles;
 
-			if(!PinSet[i].GetChild(0).transform.renderer.enabled)
+//			if(!PinSet[i].transform.GetChild(0).transform.renderer.enabled)
+//				continue;
+			if(!PinSet[i].activeSelf)
 				continue;
 
-			if((Mathf.Abs(cuurentRotation.x - defaultRotation.x) > 30 && Mathf.Abs(cuurentRotation.x - defaultRotation.x) < 330  ) ||
-			   (Mathf.Abs(cuurentRotation.y - defaultRotation.y) > 30 && Mathf.Abs(cuurentRotation.y - defaultRotation.y) < 330  ) ||
-			   (Mathf.Abs(cuurentRotation.z - defaultRotation.z) > 30 && Mathf.Abs(cuurentRotation.z - defaultRotation.z) < 330  )
+			if((Mathf.Abs(cuurentRotation.x - defaultRotation.x) > 40 && Mathf.Abs(cuurentRotation.x - defaultRotation.x) < 320  ) ||
+			   (Mathf.Abs(cuurentRotation.y - defaultRotation.y) > 40 && Mathf.Abs(cuurentRotation.y - defaultRotation.y) < 320  ) ||
+			   (Mathf.Abs(cuurentRotation.z - defaultRotation.z) > 40 && Mathf.Abs(cuurentRotation.z - defaultRotation.z) < 320  ) //||
+//			   (PinSet[i].transform.position.y < 0)
 			   ){
-				PinSet[i].GetChild(0).transform.renderer.enabled = PinSet[i].transform.collider.enabled = false;
+//				PinSet[i].GetChild(0).transform.renderer.enabled = PinSet[i].transform.collider.enabled = false;
+				PinSet[i].SetActive(false);
 				score++;
 			}
 			
@@ -181,6 +200,31 @@ public class Ball : MonoBehaviour {
 	void OnMouseDrag() 
 
 	{  
+//		Debug.Log(Mathf.Clamp(2,0,5));
+//		Debug.Log (Input.mousePosition.x);
+//		if (Input.mousePosition.x < 178) {
+//						Vector3 curScreenPoint = new Vector3 (178, /*Input.mousePosition.y*/screenPoint.y, screenPoint.z);
+//						Vector2 newPosition = new Vector2 (178, Input.mousePosition.y);
+//						velocity = (newPosition - previousPosition);
+//						previousPosition = newPosition;
+//						Vector3 curPosition = Camera.main.ScreenToWorldPoint (curScreenPoint) + offset;
+//						transform.position = new Vector3 (curPosition.x, defaultPosition.y, defaultPosition.z + curPosition.y);
+//				} else if (Input.mousePosition.x > 420) {
+//						Vector3 curScreenPoint = new Vector3 (420, /*Input.mousePosition.y*/screenPoint.y, screenPoint.z);
+//						Vector2 newPosition = new Vector2 (420, Input.mousePosition.y);
+//						velocity = (newPosition - previousPosition);
+//						previousPosition = newPosition;
+//						Vector3 curPosition = Camera.main.ScreenToWorldPoint (curScreenPoint) + offset;
+//						transform.position = new Vector3 (curPosition.x, defaultPosition.y, defaultPosition.z + curPosition.y);
+//
+//				} else {		
+//						Vector3 curScreenPoint = new Vector3 (Input.mousePosition.x, /*Input.mousePosition.y*/screenPoint.y, screenPoint.z);
+//						Vector2 newPosition = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
+//						velocity = (newPosition - previousPosition);
+//						previousPosition = newPosition;
+//						Vector3 curPosition = Camera.main.ScreenToWorldPoint (curScreenPoint) + offset;
+//						transform.position = new Vector3 (curPosition.x, defaultPosition.y, defaultPosition.z + curPosition.y);
+//				}
 		Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, /*Input.mousePosition.y*/screenPoint.y, screenPoint.z);
 		Vector2 newPosition = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
 		velocity = (newPosition - previousPosition);
@@ -193,12 +237,12 @@ public class Ball : MonoBehaviour {
 
 	void OnMouseUp()
 	{
-		if (Mathf.Abs (velocity.x) > 115) {
-			velocity = new Vector2(Mathf.Sign(velocity.x) * 115,velocity.y);
+		if (Mathf.Abs (velocity.x) > 80) {
+			velocity = new Vector2(Mathf.Sign(velocity.x) * 80,velocity.y);
 				}
 
-		if (Mathf.Abs (velocity.y) > 115) {
-			velocity = new Vector2(velocity.x,Mathf.Sign(velocity.y) * 115);
+		if (Mathf.Abs (velocity.y) > 80) {
+			velocity = new Vector2(velocity.x,Mathf.Sign(velocity.y) * 80);
 		}
 
 		if (velocity.y > 10) {
@@ -207,58 +251,4 @@ public class Ball : MonoBehaviour {
 				}
 	}
 
-//	int beganCount = 0;
-//	void TouchHandle()
-//	{
-//
-//			if (Input.touchCount == 0)
-//				return;
-//
-//		if (Input.GetTouch (0).phase == TouchPhase.Began && !IsTouching) {
-//						
-//						screenPoint = Camera.main.WorldToScreenPoint (gameObject.transform.position);
-//						offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (Input.GetTouch (0).position.x, Input.GetTouch (0).position.y, screenPoint.z));
-//						previousPosition = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
-//						IsTouching = true;
-//						Debug.Log ("Started");
-////						beganCount++;
-////						debugText.text = beganCount.ToString ();
-//	
-//
-//				}
-//
-//		if (Input.GetTouch (0).phase == TouchPhase.Moved) {
-//			
-//			Vector3 curScreenPoint = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, screenPoint.z);
-//			Vector2 newPosition = new Vector2 (Input.GetTouch(0).position.x, Input.GetTouch(0).position.y);
-//			velocity = (newPosition - previousPosition);			
-//			debugText.text = newPosition.ToString() + "  " + previousPosition.ToString();
-//			previousPosition = newPosition;
-//			Vector3 curPosition   = Camera.main.ScreenToWorldPoint(curScreenPoint)+ offset;
-//			transform.position = new Vector3(curPosition.x,defaultPosition.y, defaultPosition.z + curPosition.y);
-//			Debug.Log("Move");
-//		
-//
-//		}
-//		if (Input.GetTouch (0).phase == TouchPhase.Ended) {
-//			Debug.Log("Ended");
-//			IsTouching = false;
-//			if (Mathf.Abs (velocity.x) > 80) {
-//				velocity = new Vector2(Mathf.Sign(velocity.x) * 80,velocity.y);
-//			}
-//			if (Mathf.Abs (velocity.y) > 60) {
-//				velocity = new Vector2(velocity.x,Mathf.Sign(velocity.y) * 60);
-//			}
-//			
-//			if(velocity.y > 10)
-//				rigidbody.AddForce(new Vector3(velocity.x / 6.0f ,0,velocity.y) * 40.0f);
-//			
-//		}
-//
-//
-	 /*void OnCollisionEnter(Collision c){
-		if (c.collider.tag == "PIN") {
-			Time.timeScale = 1.5f;
-				}
-	}*/
 }
---
+++ Assets/Scripts/CameraRotate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotate : MonoBehaviour {
+
+	public GameObject Traget1;
+	public GameObject Traget2;
+	public GameObject Traget3;
+	public GameObject Traget4;
+	public GameObject TragetMiddle;
+	// Use this for initialization
+	void Start () {
+	
+		this.transform.LookAt (TragetMiddle.transform);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		this.transform.LookAt (TragetMiddle.transform);
+
+	
+	}
+}
